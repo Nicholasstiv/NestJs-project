9c6c192dbb3ace881140580f05bffe965912d4bb
Nicholasstiv1 <nicholas.guerino@gmail.com>
2026-06-25 16:34:51 -0400

    Roles-guard & Roles-decorator

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,40 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UserRole } from '../../database/prisma/enums';
+import { ROLES_KEY } from '../decorators/roles.decorators';
+import { Payload } from '../interfaces/payload.interface';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest<{ user: Payload }>();
+
+    if (!user) {
+      throw new ForbiddenException('User not authenticated');
+    }
+
+    const hasRequiredRole = requiredRoles.some((role) => user.role === role);
+
+    if (!hasRequiredRole) {
+      throw new ForbiddenException('Insufficient permission');
+    }
+
+    return true;
+  }
+}
